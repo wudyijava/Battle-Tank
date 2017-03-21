5c9baa4ee684219823c7da08ac89ad232cc749c5
wudyijava <wudayong0703@gmail.com>
2017-03-21 09:11:14 +0800

    BT03：地形材质与坦克雏形

---
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "Tank.h"
+
+
+// Sets default values
+ATank::ATank()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Turret_Clockwise", EInputEvent::IE_Pressed, this, &ATank::RotateTurret);
+}
+
+void ATank::RotateTurret() {
+	UE_LOG(LogTemp, Warning, TEXT("Turret Clockwise"));
+}
+
