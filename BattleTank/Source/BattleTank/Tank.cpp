66452d8062cb5d04a15387d12d3328255bf5bd06
wudyijava <wudayong0703@gmail.com>
2017-03-22 19:21:28 +0800

    BT04：坦克基本控制以及模型导入与材质构建

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -31,10 +31,52 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAction("Turret_Clockwise", EInputEvent::IE_Pressed, this, &ATank::RotateTurret);
+	PlayerInputComponent->BindAxis("Rotate_Turret", this, &ATank::RotateTurret);
+	PlayerInputComponent->BindAxis("Elevate_Turret", this, &ATank::RotateBarrel);
+	PlayerInputComponent->BindAxis("Move_Tank", this, &ATank::MoveTank);
+	PlayerInputComponent->BindAxis("Rotate_Tank", this, &ATank::RotateTank);
 }
 
-void ATank::RotateTurret() {
-	UE_LOG(LogTemp, Warning, TEXT("Turret Clockwise"));
+void ATank::RotateTurret(float speed) {
+	if (!turret) return;
+	auto rotation = GetWorld()->GetDeltaSeconds() * speed * rotationRate;
+	turret->AddRelativeRotation(FRotator(0, rotation, 0));
+}
+
+void ATank::RotateBarrel(float speed)
+{
+	if (!barrel) return;
+	auto rotation = GetWorld()->GetDeltaSeconds() * speed * rotationRate;
+	barrel->AddRelativeRotation(FRotator(rotation, 0, 0));
+}
+
+void ATank::MoveTank(float speed)
+{
+	if (!tank) return;
+	auto distance = GetWorld()->GetDeltaSeconds() * speed * movementRate;
+	//更新到正方向一定比例的位置为新的位置
+	tank->AddRelativeLocation(tank->GetForwardVector() * distance);
+}
+
+void ATank::RotateTank(float speed)
+{
+	if (!tank) return;
+	auto rotation = GetWorld()->GetDeltaSeconds() * speed * rotationRate;
+	tank->AddRelativeRotation(FRotator(0, rotation, 0));
+}
+
+void ATank::SetTurretChildActor(UChildActorComponent * turretFromBP)
+{
+	turret = turretFromBP;
+}
+
+void ATank::SetBarrelChildActor(UChildActorComponent * barrelFromBP)
+{
+	barrel = barrelFromBP;
+}
+
+void ATank::SetTankChildActor(UChildActorComponent * tankFromBP)
+{
+	tank = tankFromBP;
 }
 
--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -24,7 +24,29 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	void RotateTurret();
 	
+	//通过蓝图注入炮塔参数
+	UFUNCTION(BlueprintCallable, Category = Tank)
+	void SetTurretChildActor(UChildActorComponent* turretFromBP);
+
+	//通过蓝图注入炮管参数
+	UFUNCTION(BlueprintCallable, Category = Tank)
+	void SetBarrelChildActor(UChildActorComponent* barrelFromBP);
+
+	//通过蓝图注入坦克参数
+	UFUNCTION(BlueprintCallable, Category = Tank)
+	void SetTankChildActor(UChildActorComponent* tankFromBP);
+private:
+	UChildActorComponent* turret;	//存放炮塔对象
+	UChildActorComponent* barrel;	//存放炮管对象
+	UChildActorComponent* tank;		//存放坦克车身对象
+	UPROPERTY(EditAnywhere)
+	float movementRate = 1200.0f;		//车身移动速率
+	UPROPERTY(EditAnywhere)
+	float rotationRate = 120.0f;		//车身转向速率
+
+	void RotateTurret(float);	//旋转炮塔
+	void RotateBarrel(float);	//旋转炮管
+	void MoveTank(float);		//车身前后移动
+	void RotateTank(float);		//车身转向
 };
